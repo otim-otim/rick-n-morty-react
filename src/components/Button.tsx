0e1fe03133b2c2a34f8d64e13e36f19ecadd1914
otimIam <otim.idb@gmail.com>
2023-11-12 23:48:35 +0300

    put click effect on button compoonent

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 
 interface Props {
   label: 'Next' | 'Previous'
+  // clickFunction : Function 
 }
 const Button = ({label} : Props) => {
+
+
   return (
-    <>
+    <div>
        
-        <button className="btn btn-primary">{label}</button>
-    </>
+        <button className="btn btn-primary" >{label}</button>
+    </div>
   )
 }
 
